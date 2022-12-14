3ec8b5c724e22929ba78241aec2cf412de5e1269
Jesus <104527491+FuryBaco@users.noreply.github.com>
2022-12-14 09:05:32 +0000

    Dibujando el creeper de Minecraft

--- script.js
+++ script.js
@@ -1,67 +1,14 @@
-var pantalla = document.querySelector('canvas');
-var pincel = pantalla.getContext('2d');
-var brushColor = "black";
-
-
-pincel.fillStyle = 'grey';
-pincel.fillRect(0, 0, 600, 400);
-
-pincel.fillStyle = "red";
-pincel.fillRect(0,0,50,50);
-
-pincel.fillStyle = "green";
-pincel.fillRect(50,0,50,50);
-
-pincel.fillStyle = "blue";
-pincel.fillRect(100,0,50,50);
-
-var puedoDibujar = false;
-
-function dibujarCirculo(evento) {
-
-  if (puedoDibujar) {
-    var x = evento.pageX - pantalla.offsetLeft;
-    var y = evento.pageY - pantalla.offsetTop;
-    if ((x > 150 || y > 50)){
-    pincel.fillStyle = brushColor;
-    pincel.beginPath();
-    pincel.arc(x, y, 5, 0, 2 * 3.14);
-    pincel.fill();
-    }
-  }
-
-}
-
-function chooseColor(evento){
-  var x = evento.pageX - pantalla.offsetLeft;
-  var y = evento.pageY - pantalla.offsetTop;
-  console.log(x,y) ;
-  if ((x >= 0 && x <= 50) && (y >= 0 && y <= 50)){
-    brushColor = "red";
-  } else if ((x > 50 && x <= 100) && (y >= 0 && y <= 50)){
-    brushColor = "green";
-  } else if ((x > 100 && x <= 150) && (y >= 0 && y <= 50)){
-    brushColor = "blue";
-  }
-}
-
-
-
-pantalla.onclick = chooseColor;
-
-pantalla.onmousemove = dibujarCirculo;
-
-function habilitarDibujar() {
-
-  puedoDibujar = true;
-}
-
-function deshabilitarDibujar() {
-
-  puedoDibujar = false;
-}
-
-pantalla.onmousedown = habilitarDibujar;
-
-pantalla.onmouseup = deshabilitarDibujar;
-
+var pantalla = document.querySelector("canvas");
+var pincel = pantalla.getContext("2d");
+//cabeza
+pincel.fillStyle = 'darkgreen';
+pincel.fillRect(200, 50, 350, 300);
+//ojos
+pincel.fillStyle = 'black';
+pincel.fillRect(250, 110, 90, 90);
+pincel.fillRect(410, 110, 90, 90);
+//nariz
+pincel.fillRect(340, 200, 70, 100);
+//boca
+pincel.fillRect(300, 240, 40, 110);
+pincel.fillRect(410, 240, 40, 110);
