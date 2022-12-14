704742b20e9024ab435c372c5fa5c77e4f2edb28
Jesus <104527491+FuryBaco@users.noreply.github.com>
2022-12-14 09:02:49 +0000

    Dibujando un circulo

--- script.js
+++ script.js
@@ -1,67 +1,20 @@
-var pantalla = document.querySelector('canvas');
-var pincel = pantalla.getContext('2d');
-var brushColor = "black";
-
-
-pincel.fillStyle = 'grey';
+var pantalla = document.querySelector("canvas");
+var pincel = pantalla.getContext("2d");
+pincel.fillStyle = "lightgray";
 pincel.fillRect(0, 0, 600, 400);
 
-pincel.fillStyle = "red";
-pincel.fillRect(0,0,50,50);
+function dibujarCirculo(x, y, radio, color) {
 
-pincel.fillStyle = "green";
-pincel.fillRect(50,0,50,50);
-
-pincel.fillStyle = "blue";
-pincel.fillRect(100,0,50,50);
-
-var puedoDibujar = false;
-
-function dibujarCirculo(evento) {
-
-  if (puedoDibujar) {
-    var x = evento.pageX - pantalla.offsetLeft;
-    var y = evento.pageY - pantalla.offsetTop;
-    if ((x > 150 || y > 50)){
-    pincel.fillStyle = brushColor;
-    pincel.beginPath();
-    pincel.arc(x, y, 5, 0, 2 * 3.14);
-    pincel.fill();
-    }
-  }
-
+  pincel.fillStyle = color;
+  pincel.beginPath();
+  pincel.arc(x, y, radio, 0, 2 * 3.14);
+  pincel.fill();
 }
-
-function chooseColor(evento){
-  var x = evento.pageX - pantalla.offsetLeft;
-  var y = evento.pageY - pantalla.offsetTop;
-  console.log(x,y) ;
-  if ((x >= 0 && x <= 50) && (y >= 0 && y <= 50)){
-    brushColor = "red";
-  } else if ((x > 50 && x <= 100) && (y >= 0 && y <= 50)){
-    brushColor = "green";
-  } else if ((x > 100 && x <= 150) && (y >= 0 && y <= 50)){
-    brushColor = "blue";
-  }
+function dibujarFlor(x, y) {
+  dibujarCirculo(x, y + 20, 10, "blue");
+  dibujarCirculo(x, y, 10, "red");
+  dibujarCirculo(x, y - 20, 10, "yellow");
+  dibujarCirculo(x - 20, y, 10, "orange");
+  dibujarCirculo(x + 20, y, 10, "black");
 }
-
-
-
-pantalla.onclick = chooseColor;
-
-pantalla.onmousemove = dibujarCirculo;
-
-function habilitarDibujar() {
-
-  puedoDibujar = true;
-}
-
-function deshabilitarDibujar() {
-
-  puedoDibujar = false;
-}
-
-pantalla.onmousedown = habilitarDibujar;
-
-pantalla.onmouseup = deshabilitarDibujar;
-
+dibujarFlor(300, 200);
