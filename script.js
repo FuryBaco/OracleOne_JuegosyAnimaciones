6985310a4c003c693cc856b418ee5fd06244ff9a
Jesus <104527491+FuryBaco@users.noreply.github.com>
2022-12-14 08:57:56 +0000

    Replica del programa_4 adicionando el reto de que la bolita cambie de dirección al llegar al borde del canvas

--- script.js
+++ script.js
@@ -1,67 +1,35 @@
-var pantalla = document.querySelector('canvas');
-var pincel = pantalla.getContext('2d');
-var brushColor = "black";
+var pantalla = document.querySelector("canvas");
+var pincel = pantalla.getContext("2d");
 
-
-pincel.fillStyle = 'grey';
+pincel.fillStyle = "grey";
 pincel.fillRect(0, 0, 600, 400);
 
-pincel.fillStyle = "red";
-pincel.fillRect(0,0,50,50);
+function dibujarCircunferencia(x, y, radio) {
+  pincel.fillStyle = "blue";
+  pincel.beginPath();
+  pincel.arc(x, y, radio, 0, 2 * Math.PI);
+  pincel.fill();
+}
 
-pincel.fillStyle = "green";
-pincel.fillRect(50,0,50,50);
+function limpiarPantalla() {
+  pincel.clearRect(0, 0, 600, 400);
+}
 
-pincel.fillStyle = "blue";
-pincel.fillRect(100,0,50,50);
+var x = 0;
+var sentido = 1;
 
-var puedoDibujar = false;
-
-function dibujarCirculo(evento) {
-
-  if (puedoDibujar) {
-    var x = evento.pageX - pantalla.offsetLeft;
-    var y = evento.pageY - pantalla.offsetTop;
-    if ((x > 150 || y > 50)){
-    pincel.fillStyle = brushColor;
-    pincel.beginPath();
-    pincel.arc(x, y, 5, 0, 2 * 3.14);
-    pincel.fill();
-    }
+function actualizarPantalla(){
+  limpiarPantalla();
+  if (x >= pantalla.width){
+    sentido = -1;
+  } else if(x <= 0 ){
+    sentido = 1;
   }
-
-}
-
-function chooseColor(evento){
-  var x = evento.pageX - pantalla.offsetLeft;
-  var y = evento.pageY - pantalla.offsetTop;
-  console.log(x,y) ;
-  if ((x >= 0 && x <= 50) && (y >= 0 && y <= 50)){
-    brushColor = "red";
-  } else if ((x > 50 && x <= 100) && (y >= 0 && y <= 50)){
-    brushColor = "green";
-  } else if ((x > 100 && x <= 150) && (y >= 0 && y <= 50)){
-    brushColor = "blue";
-  }
+  dibujarCircunferencia(x, 20, 10);
+  x += sentido;
 }
 
 
+setInterval(actualizarPantalla, 10);
 
-pantalla.onclick = chooseColor;
-
-pantalla.onmousemove = dibujarCirculo;
-
-function habilitarDibujar() {
-
-  puedoDibujar = true;
-}
-
-function deshabilitarDibujar() {
-
-  puedoDibujar = false;
-}
-
-pantalla.onmousedown = habilitarDibujar;
-
-pantalla.onmouseup = deshabilitarDibujar;
 
