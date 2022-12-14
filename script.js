ddd60969aefe750a2909663ba0bf0056ea8cbbe8
Jesus <104527491+FuryBaco@users.noreply.github.com>
2022-12-14 08:59:34 +0000

    Replica del programa 3

--- script.js
+++ script.js
@@ -1,67 +1,39 @@
-var pantalla = document.querySelector('canvas');
-var pincel = pantalla.getContext('2d');
-var brushColor = "black";
+var pantalla = document.querySelector("canvas");
+var pincel = pantalla.getContext("2d");
 
-
-pincel.fillStyle = 'grey';
+pincel.fillStyle = "grey";
 pincel.fillRect(0, 0, 600, 400);
+var colores = ["blue", "red", "green"];
+var indiceColorActual = 0; // comienza con blue
 
-pincel.fillStyle = "red";
-pincel.fillRect(0,0,50,50);
-
-pincel.fillStyle = "green";
-pincel.fillRect(50,0,50,50);
-
-pincel.fillStyle = "blue";
-pincel.fillRect(100,0,50,50);
-
-var puedoDibujar = false;
-
-function dibujarCirculo(evento) {
-
-  if (puedoDibujar) {
-    var x = evento.pageX - pantalla.offsetLeft;
-    var y = evento.pageY - pantalla.offsetTop;
-    if ((x > 150 || y > 50)){
-    pincel.fillStyle = brushColor;
-    pincel.beginPath();
-    pincel.arc(x, y, 5, 0, 2 * 3.14);
-    pincel.fill();
-    }
-  }
-
-}
-
-function chooseColor(evento){
+function exhibirAlerta(evento) {
   var x = evento.pageX - pantalla.offsetLeft;
   var y = evento.pageY - pantalla.offsetTop;
-  console.log(x,y) ;
-  if ((x >= 0 && x <= 50) && (y >= 0 && y <= 50)){
-    brushColor = "red";
-  } else if ((x > 50 && x <= 100) && (y >= 0 && y <= 50)){
-    brushColor = "green";
-  } else if ((x > 100 && x <= 150) && (y >= 0 && y <= 50)){
-    brushColor = "blue";
+  console.log(evento);
+  alert("El click que hizo esta en las coordenadas " + x + "," + y)
+}
+
+function dibujarCirculo(evento, color) {
+  var x = evento.pageX - pantalla.offsetLeft;
+  var y = evento.pageY - pantalla.offsetTop;
+
+
+  pincel.fillStyle = colores[indiceColorActual];
+  pincel.beginPath();
+  pincel.arc(x, y, 10, 0, 2 * 3.1416);
+  pincel.fill();
+
+  console.log("El click que hizo esta en las coordenadas " + x + "," + y);
+
+}
+
+function alterarColor() {
+  indiceColorActual++;
+  if (indiceColorActual >= colores.length) {
+    indiceColorActual = 0; //vuelve para el primer color, blue
   }
+  return false; //menú contextual padrón de `canvas` no sea exhibido
 }
 
-
-
-pantalla.onclick = chooseColor;
-
-pantalla.onmousemove = dibujarCirculo;
-
-function habilitarDibujar() {
-
-  puedoDibujar = true;
-}
-
-function deshabilitarDibujar() {
-
-  puedoDibujar = false;
-}
-
-pantalla.onmousedown = habilitarDibujar;
-
-pantalla.onmouseup = deshabilitarDibujar;
-
+pantalla.oncontextmenu = alterarColor;
+pantalla.onclick = dibujarCirculo;
